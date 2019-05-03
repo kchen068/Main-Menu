a52404bd379fba15aa32097f00ddb8840f075a82
Kevin <kchen068@ucr.edu>
2019-05-02 18:20:25 -0700

    First oush

---
+++ Assets/Scripts/BrightSlide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrightSlide : MonoBehaviour
+{
+    public float GammaCorrection;
+    //public Rect SliderLocation;
+    public Slider bright;
+
+    void Start()
+    {
+        GammaCorrection = 0.55f;
+        RenderSettings.ambientLight = new Color(GammaCorrection, GammaCorrection, GammaCorrection, 1.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        GammaCorrection = bright.value;
+        RenderSettings.ambientLight = new Color(GammaCorrection, GammaCorrection, GammaCorrection, 1.0f);
+
+    }
+}
